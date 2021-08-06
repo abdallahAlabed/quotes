3d33572c6dc085788039e2f0a6c7c785f2afcb6a
abdallahAlabed <81149825+abdallahAlabed@users.noreply.github.com>
2021-08-06 15:24:38 +0300

    Update App.java
    last App update

--- app/src/main/java/quotes/App.java
+++ app/src/main/java/quotes/App.java
@@ -1,6 +1,7 @@
 package quote;
 
 import com.google.gson.Gson;
+
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
@@ -10,43 +11,43 @@ import java.util.Arrays;
 public class App {
     public static void main(String[] args) throws IOException {
         System.out.println(Quotes.randomQuote("recentquotes.json"));
-        
+        System.out.println("------------------");
+
         String apiURL = "http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en";
-try {
-    URL url = new URL(apiURL);
-    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-    connection.setRequestMethod("GET");
-    connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
-    int status = connection.getResponseCode();
+        try {
+            URL url = new URL(apiURL);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
+            int status = connection.getResponseCode();
 
-    if (status == 200) {
-        InputStream inputStream = connection.getInputStream();
-        InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-        String line = bufferedReader.readLine();
-        while (line != null) {
-            System.out.println(line);
-            line = bufferedReader.readLine();
-        }
+            if (status == 200) {
+                InputStream inputStream = connection.getInputStream();
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                String line = bufferedReader.readLine();
+                while (line != null) {
+                    System.out.println(line);
+                    line = bufferedReader.readLine();
+                }
 //                System.out.println(line);
-        bufferedReader.close();
+                bufferedReader.close();
 
-        Gson gson = new Gson();
+                Gson gson = new Gson();
 
-        Quotes[] s = gson.fromJson(line, Quotes[].class);
-        System.out.println(Arrays.toString(s));
-    } else {
-        System.out.println("An error occurred with status " + status);
-    }
+                Quotes[] s = gson.fromJson(line, Quotes[].class);
+                System.out.println(Arrays.toString(s));
+            } else {
+                System.out.println("An error occurred with status " + status);
+            }
 
-    connection.disconnect();
+            connection.disconnect();
 
-} catch (MalformedURLException e) {
-    e.printStackTrace();
-} catch (IOException e) {
-    e.printStackTrace();
-}
-
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
